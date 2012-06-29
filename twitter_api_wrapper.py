b6eb841418ff63be1e1baf2fa8ee3fc38fa70fe4
yuriks <yuriks@yuriks.net>
2012-06-29 00:31:32 -0300

    More robust URL handling.

--- twitter_api_wrapper.py
+++ twitter_api_wrapper.py
@@ -1,11 +1,12 @@
 import json
+from urllib.parse import urlencode
 from util import response_to_json, build_arg_string
 
 def get_twitter_id(username):
-	twitter_api_url = 'https://api.twitter.com/1/users/show.json?screen_name=' + username
-	json_output = response_to_json(twitter_api_url)
-	return json_output['id'] if json_output else None
-	
+    twitter_api_url = "https://api.twitter.com/1/users/show.json?"
+    json_output = response_to_json(twitter_api_url + urlencode({'screen_name': username}))
+    return json_output['id'] if json_output else None
+
 def get_tweets(user_id, n=10):
 	args = {'id' : str(user_id), 'count' : str(n)}
 	arg_str = build_arg_string(args)
