8b992a6e91a55193ed748e5d797b99d1fd688a21
yuriks <yuriks@yuriks.net>
2012-06-28 23:56:31 -0300

    Made -u/--user argument required.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from twitter_api_wrapper import get_twitter_id, get_tweets
 
 def setup_arguments():
 	parser = ArgumentParser(description='Provides you a automated stalking tool!')
-	parser.add_argument('-u', '--user', dest = 'user', metavar='USER', type=str, 
+	parser.add_argument('-u', '--user', dest='user', metavar='USER', type=str, required=True,
 		help='The stalking target user!')
 	return parser
 	
