2adc03c76bdef9016329db0debbc0644ad39ec6e
yuriks <yuriks@yuriks.net>
2012-06-29 00:56:07 -0300

    Replaced bad url encoding code with urlencode.

--- twitter_api_wrapper.py
+++ twitter_api_wrapper.py
@@ -1,6 +1,6 @@
 import json
 from urllib.parse import urlencode
-from util import response_to_json, build_arg_string
+from util import response_to_json
 
 def get_twitter_id(username):
     twitter_api_url = "https://api.twitter.com/1/users/show.json?"
@@ -8,8 +8,7 @@ def get_twitter_id(username):
     return json_output['id'] if json_output else None
 
 def get_tweets(user_id, n=10):
-    args = {'id' : str(user_id), 'count' : str(n)}
-    arg_str = build_arg_string(args)
+    arg_str = urlencode({'id' : str(user_id), 'count' : str(n)})
 
     api_timeline_url = 'https://api.twitter.com/1/statuses/user_timeline.json?' + arg_str
     json_output = response_to_json(api_timeline_url)
