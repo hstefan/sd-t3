a5e962929439769192ee46495a6fb24940af5df7
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-06-29 02:40:19 -0300

    Displaying only tweets with location and asking user which one he wants to follow. Follow as in chasing.

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ def setup_arguments():
     parser = ArgumentParser(description='Provides you with an automated stalking tool!')
     parser.add_argument('-u', '--user', dest='user', metavar='USER', type=str, required=True,
         help='The stalking target user!')
+    parser.add_argument('-n', '--num_tweets', dest='num_tweets', metavar='NUM_TWEETS', type=int, required=True,
+        help='Number of tweets to retrieve')
     return parser
 
 def display_tweets(tweets_json):
+    with_location = []
     for i, tweet in enumerate(tweets_json):
-        message = '(' + str(i+1) + ') ' + tweet['text'] + '\n'
-        # Prevent a failed encode from blowing up the program
-        # This is just output after all.
-        sys.stdout.buffer.write(message.encode(sys.stdout.encoding, 'replace'))
+        geo_enabled = tweet['user']['geo_enabled']
+        coordinates = tweet['coordinates']
+        if geo_enabled and  coordinates:
+            message = '(' + str(i+1) + ') ' + tweet['text'] + '\n'
+            # Prevent a failed encode from blowing up the program
+            # This is just output after all.
+            sys.stdout.buffer.write(message.encode(sys.stdout.encoding, 'replace'))
+            with_location.append((message, coordinates))
+    return with_location
 
 def main():
     parser = setup_arguments()
@@ -22,7 +30,14 @@ def main():
     user_str = args.user
     id = get_twitter_id(user_str)
 
-    display_tweets(get_tweets(id, 10))
-
+    with_location = display_tweets(get_tweets(id, args.num_tweets))
+    
+    tweet_selected = None
+    while not tweet_selected:
+        try:
+            tweet_selected = with_location[eval(input('Numero do tweet a ser seguido: ')) -1]
+        except IndexError:
+            print('Indice invalido.')
+    
 if __name__ == '__main__':
     main()
