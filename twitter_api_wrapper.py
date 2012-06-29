9dfbcc85085ff0ed0c14ccd65820d083760cf9fe
yuriks <yuriks@yuriks.net>
2012-06-29 00:24:30 -0300

    Handle missing users better.

--- twitter_api_wrapper.py
+++ twitter_api_wrapper.py
@@ -4,7 +4,7 @@ from util import response_to_json, build_arg_string
 def get_twitter_id(username):
 	twitter_api_url = 'https://api.twitter.com/1/users/show.json?screen_name=' + username
 	json_output = response_to_json(twitter_api_url)
-	return json_output['id']
+	return json_output['id'] if json_output else None
 	
 def get_tweets(user_id, n=10):
 	args = {'id' : str(user_id), 'count' : str(n)}
--- util.py
+++ util.py
@@ -1,13 +1,21 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, HTTPError
 import json
 
 def response_to_json(url, enc='utf-8'):
-	response = urlopen(url)
-	raw_data = response.read()
-	text_data = raw_data.decode(enc)
-	json_output = json.loads(text_data)
-	return json_output
-	
+    try:
+        response = urlopen(url)
+    except HTTPError as e:
+        if e.code == 404:
+            return None
+        else:
+            raise
+
+    raw_data = response.read()
+    # TODO Determine proper encoding from HTTP headers
+    text_data = raw_data.decode(enc)
+    json_output = json.loads(text_data)
+    return json_output
+
 def build_arg_string(args, sep='&'):
 	arg_str = ''
 	for k, v in args.items():
