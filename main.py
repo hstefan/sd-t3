c417d9451ec6a488318621655131227eb1ca409e
yuriks <yuriks@yuriks.net>
2012-06-29 00:58:29 -0300

    Removed random leading whitespace lines.

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ def setup_arguments():
     parser.add_argument('-u', '--user', dest='user', metavar='USER', type=str, required=True,
         help='The stalking target user!')
     return parser
-	
+
 def display_tweets(tweets_json):
     i = '0'
     for tweet in tweets_json:
         print('(' + str(i) + ') ' + tweet['text'])
-	
+
 def main():
     parser = setup_arguments()
     args = parser.parse_args()	
     user_str = args.user
     id = get_twitter_id(user_str)
     display_tweets(get_tweets(id, 10))
-	
+
 if __name__ == '__main__':
     main()
