a62e5ff55e1dc0f1a7c4b78bde8ad49f7867bb92
yuriks <yuriks@yuriks.net>
2012-06-28 23:41:44 -0300

    Clarified variable name.

--- util.py
+++ util.py
@@ -4,6 +4,6 @@ import json
 def response_to_json(url, enc='utf-8'):
 	response = urlopen(url)
 	raw_data = response.read()
-	dec_raw_data = raw_data.decode(enc)
-	json_output = json.loads(dec_raw_data)
+	text_data = raw_data.decode(enc)
+	json_output = json.loads(text_data)
 	return json_output
