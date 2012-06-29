4a16d4d40cf207aa772e73661daae715c3761a1e
yuriks <yuriks@yuriks.net>
2012-06-29 01:37:55 -0300

    Removed build_arg_string, again.

--- util.py
+++ util.py
@@ -20,9 +20,3 @@ def response_to_json(url, enc='utf-8'):
     text_data = raw_data.decode(enc)
     json_output = json.loads(text_data)
     return json_output
-
-def build_arg_string(args, sep='&'):
-    arg_str = ''
-    for k, v in args.items():
-        arg_str = arg_str + k + '=' + v + sep
-    return arg_str
